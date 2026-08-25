b29bb6dfabdc149cc7eb844ee75a4bc4982bc0df
marifracarolis2-arch <marifracarolis2@gmail.com>
2026-08-25 14:51:35 -0300

    Model Ingrediente feita

---
+++ Simmer/Models/Ingrediente.swift
@@ -0,0 +1,31 @@
+//
+//  IngredienteSwiftData.swift
+//  Simmer
+//
+//  Created by Mariana Fracaroli Lopes on 25/08/26.
+//
+import Foundation
+import SwiftData
+
+@Model
+final class Ingrediente {
+    
+    @Attribute(.unique)
+    var id: UUID
+    var nome: String
+    var quantidade: Double
+    var unidade: String
+    
+    init(
+            id: UUID = UUID(),
+            nome: String,
+            quantidade: Double,
+            unidade: String
+        ) {
+            self.id = id
+            self.nome = nome
+            self.quantidade = quantidade
+            self.unidade = unidade
+        }
+    
+}
